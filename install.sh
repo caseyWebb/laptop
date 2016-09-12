50753baeb749e9b77c2f20f19e3fa0865c8d2ccf
Casey Webb <caseywebb@MacBook-Pro.local>
2016-09-12 16:05:33 -0400

    wip

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+if ! command -v brew >/dev/null 2>&1; then
+  echo "Installing homebrew..."
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+  echo "Installing homebrew-cask..."
+  brew tap caskroom/cask
+else
+  echo "\`brew\` found in \$PATH. Skipping."
+fi
+
+brew install git
+
 if [ ! -d "$HOME/.laptop" ]; then
-  curl https://github.com/caseyWebb/laptop.tar.gz | tar xf -C $HOME/.laptop
+  git clone https://github.com/caseyWebb/laptop.git
 fi
