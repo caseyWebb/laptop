48a300640914dacea058e4b76a9ceef83ab8da4a
Casey Webb <notcaseywebb@gmail.com>
2016-09-13 12:53:01 -0400

    run backups detached

--- .zshrc
+++ .zshrc
@@ -4,17 +4,17 @@ export LANGUAGE=en_US.UTF-8
 
 export DISPLAY=":0.0"
 
-export NPM_PACKAGES="${HOME}/.npm"
+export NPM_PACKAGES=~/.npm
 export NODE_PATH="$NPM_PACKAGES/lib/node_modules"
 export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$NPM_PACKAGES/bin"
-export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"
+export N_PREFIX=~/n; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"
 
 export ZSH=~/.oh-my-zsh
 export ZSH_THEME="custom"
-export ENABLE_CORRECTION="true"
-export COMPLETION_WAITING_DOTS="true"
-export DISABLE_UNTRACKED_FILES_DIRTY="false"
-export COMPLETION_WAITING_DOTS="true"
+export ENABLE_CORRECTION=true
+export COMPLETION_WAITING_DOTS=true
+export DISABLE_UNTRACKED_FILES_DIRTY=false
+export COMPLETION_WAITING_DOTS=true
 export DISABLE_UPDATE_PROMPT=true
 
 source $ZSH/oh-my-zsh.sh
@@ -26,7 +26,7 @@ ssh-add ~/.ssh/id_rsa &>/dev/null
 . `brew --prefix`/etc/profile.d/z.sh
 
 source ~/.laptop/scripts/20-aliases.sh
-source ~/.laptop/scripts/20-sync-atom-packages.sh
-source ~/.laptop/scripts/20-sync-brew-packages.sh
+screen -dm -S Shared ~/.laptop/scripts/20-sync-atom-packages.sh
+screen -dm -S Shared ~/.laptop/scripts/20-sync-brew-packages.sh
 
 cd ~
--- scripts/20-sync-brew-packages.sh
+++ scripts/20-sync-brew-packages.sh
@@ -17,6 +17,16 @@ function _upgrade_brew() {
   _update_brew_update
 }
 
+brew list > ~/.laptop/brew-packages.txt
+brew cask list > ~/.laptop/brew-cask-packages.txt
+
+cd ~/.laptop
+if git status | grep -q brew-packages\.txt || git status | grep -q brew-cask-packages\.txt; then
+  git add brew-packages.txt
+  git add brew-cask-packages.txt
+  sh -c "cd ~/.laptop && git commit -m \"[automated] sync brew (cask) packages\" && git push" 2>&1 > /dev/null
+fi
+
 if [ -f ~/.brew-update ]; then
   . ~/.brew-update
 
@@ -27,24 +37,8 @@ if [ -f ~/.brew-update ]; then
   epoch_diff=$(($(_current_epoch) - $LAST_EPOCH))
 
   if [ $epoch_diff -gt 1 ]; then
-    echo "[Homebrew] Would you like to check for updates? [Y/n]: \c"
-    read line
-    if [[ "$line" == Y* ]] || [[ "$line" == y* ]] || [ -z "$line" ]; then
-      _upgrade_brew
-    else
-      _update_brew_update
-    fi
+    _upgrade_brew &
   fi
 else
   _update_brew_update
-fi
-
-brew list > ~/.laptop/brew-packages.txt
-brew cask list > ~/.laptop/brew-cask-packages.txt
-
-cd ~/.laptop
-if git status | grep -q brew-packages\.txt || git status | grep -q brew-cask-packages\.txt; then
-  git add brew-packages.txt
-  git add brew-cask-packages.txt
-  sh -c "cd ~/.laptop && git commit -m \"[automated] sync brew (cask) packages\" && git push" 2>&1 > /dev/null
 fi
