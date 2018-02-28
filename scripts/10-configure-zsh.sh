4e7d435e96149c8fb6df9b1694cc209a4321e27f
Casey Webb <notcaseywebb@gmail.com>
2018-02-27 23:49:50 -0600

    zsh theme fixup

--- scripts/10-configure-zsh.sh
+++ scripts/10-configure-zsh.sh
@@ -16,11 +16,8 @@ else
   echo "oh-my-zsh is already installed."
 fi
 
-mkdir -p ~/.oh-my-zsh/custom/themes
-
 if [ ! -a ~/.zshrc ]; then
-  echo "Linking .zshrc and .zsh-theme..."
+  echo "Linking .zshrc..."
   ln -s ~/.laptop/.zshrc ~/.zshrc
-  ln -s ~/.laptop/.zsh-theme ~/.oh-my-zsh/custom/themes/custom.zsh-theme
   echo "Done."
 fi
