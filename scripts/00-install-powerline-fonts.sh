b2547783a6a4da2d1f1e700f387570ba9c95e4aa
Casey Webb <notcaseywebb@gmail.com>
2018-02-27 23:48:54 -0600

    :fire: Remove powerline fonts install script
    Installed via homebrew/fonts

--- scripts/00-install-powerline-fonts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p $TMPDIR/powerline-fonts
-git clone https://github.com/powerline/fonts.git $TMPDIR/powerline-fonts
-chmod +x $TMPDIR/powerline-fonts/install.sh
-$TMPDIR/powerline-fonts/install.sh
-rm -rf $TMPDIR/powerline-fonts
